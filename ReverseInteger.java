8cdfbca5c4ecbd136c9d55e9a8477ccb9233b3ff
givenwong <givenwong@gmail.com>
2014-04-24 12:25:43 -0500

    Reverse Integer
    Reverse digits of an integer.
    
    Example1: x = 123, return 321
    Example2: x = -123, return -321
    
    click to show spoilers.
    
    Have you thought about this?
    Here are some good questions to ask before coding. Bonus points for you
    if you have already thought through this!
    
    If the integer's last digit is 0, what should the output be? ie, cases
    such as 10, 100.
    
    Did you notice that the reversed integer might overflow? Assume the
    input is a 32-bit integer, then the reverse of 1000000003 overflows. How
    should you handle such cases?
    
    Throw an exception? Good, but what if throwing an exception is not an
    option? You would then have to re-design the function (ie, add an extra
    parameter).

---
+++ ReverseInteger.java
@@ -0,0 +1,33 @@
+public class ReverseInteger {
+    public int reverse(int x) {
+        
+        boolean positive = true;
+        String myString;
+        int result =0;
+        
+        //Convert the integer to string
+        
+        if(x < 0) {
+            x *=-1;
+            positive = false;
+        }
+        
+        myString = Integer.toString(x);
+        
+        
+        //Reverse the string
+        myString = new StringBuffer(myString).reverse().toString();
+        
+        
+        // Convert the string to integer
+        
+        for(int i = 0; i< myString.length(); i++){
+            if(result*10+myString.charAt(i) >= result)    // to prevent the overflow
+            result = result*10 + myString.charAt(i)-'0';
+            else System.exit(1);
+        }
+        
+        result = (positive)? result: -1*result;
+        return result;
+    }
+}

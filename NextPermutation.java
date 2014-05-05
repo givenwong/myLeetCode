d007a848425c18bb27e416ce3d9580393c027546
givenwong <givenwong@gmail.com>
2014-05-05 16:53:51 -0500

    Next Permutation

--- NextPermutation.java
+++ NextPermutation.java
@@ -1,3 +1,17 @@
+/*
+
+Step 1: Find the largest index k, such that A[k]<A[k+1]. If not exist, this is the last permutation. (in this    problem just sort the vector and return.)
+Step 2: Find the largest index l, such that A[l]>A[k].
+Step 3: Swap A[k] and A[l].
+Step 4: Reverse A[k+1] to the end.
+
+
+*/
+
+
+
+
+
 public class NextPermutation {
     public void nextPermutation(int[] num) {
         

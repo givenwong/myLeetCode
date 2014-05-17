6aabe77b3d8d6d04af1bcabcd8bba5ecda9db00c
givenwong <givenwong@gmail.com>
2014-05-17 18:13:00 -0500

    Linked list cycle ii
    find the start of the cycle

---
+++ LinkedListCycleII.java
@@ -0,0 +1,40 @@
+
+public class LinkedListCycleII  {
+    public ListNode detectCycle(ListNode head) {
+        
+        ListNode result= null;
+        
+        if(head == null || head.next == null)
+            return null;
+            
+        ListNode fast = head;
+        ListNode slow = head;
+            
+        while(fast != null && fast.next != null){
+            
+            slow = slow.next;
+            fast = fast.next.next;
+            
+            if(slow == fast)
+                break;
+            
+        }    
+        
+        if(slow == null || fast == null)
+         return null;
+        
+        ListNode aux = head;
+        
+        
+        while(aux != null && slow != null && aux != slow ){
+            aux = aux.next;
+            slow = slow.next;
+        
+        }
+   
+       if(aux == slow)
+      return aux;        
+       else 
+       return null;
+    }
+}

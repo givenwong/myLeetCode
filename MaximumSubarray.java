b21d89c804603a788ef74d771f0b7adda78dd3d6
givenwong <givenwong@gmail.com>
2014-05-19 17:42:41 -0500

    Max Subarray

---
+++ MaximumSubarray.java
@@ -0,0 +1,24 @@
+public class MaximumSubarray  {
+    public int maxSubArray(int[] A) {
+        
+        int max =Integer.MIN_VALUE;
+        int scanner =0;
+        int temp =0;
+        
+        while(scanner < A.length){
+             
+             temp += A[scanner];
+             max = (max > temp)?max:temp; 
+              
+            if(temp >=0){
+                scanner++;
+            }else{
+                scanner++;
+                temp =0;
+            }
+            
+        }
+        
+        return max;
+    }
+}

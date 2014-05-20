2f246d00e18b13bfac6bf6bc8a21529de1e02138
givenwong <givenwong@gmail.com>
2014-05-20 16:24:41 -0500

    RotateList

---
+++ RotateList.java
@@ -0,0 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class RotateList {
+    public ListNode rotateRight(ListNode head, int n) {
+        
+        if(head == null)
+        return head;
+        
+        int length =0;
+        ListNode scanner = head;
+        
+        while(scanner != null){
+            length++;
+            scanner = scanner.next;
+        }
+        
+        int displacement = n%length;
+        
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        while(displacement>0){
+            displacement--;
+            fast = fast.next;
+        }
+        
+        while(fast.next != null){
+            fast = fast.next;
+            slow = slow.next;
+        }
+        
+        fast.next = head;
+        head = slow.next;
+        
+        slow.next = null;
+        
+        
+        return head;
+    }
+}

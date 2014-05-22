b71ec623cf20bdd6a4714c25493c20a9e5ca9f91
givenwong <givenwong@gmail.com>
2014-05-21 21:48:30 -0500

    Single Number

---
+++ SingleNumber.java
@@ -0,0 +1,11 @@
+public class SingleNumber {
+    public int singleNumber(int[] A) {
+        
+        int result =0;
+        for(int i =0; i< A.length;i++)
+           result ^=A[i];
+            
+        return result;
+        
+    }
+}

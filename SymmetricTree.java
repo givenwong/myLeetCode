6201b4d45cb5e9bd0f1c8cc2b23c6bfedae6431e
givenwong <givenwong@gmail.com>
2014-05-24 19:26:37 -0500

    some modification

---
+++ SymmetricTree.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class SymmetricTree {
+    public boolean isSymmetric(TreeNode root) {
+        
+        if(root == null)
+        return true;
+        
+      return isMirror(root.left, root.right);
+        
+        
+    }
+    
+    public boolean isMirror(TreeNode rootOne, TreeNode rootTwo){
+    
+        if(rootOne == null)
+        return rootTwo == null;
+        if(rootTwo == null)
+        return rootOne == null;
+    
+        boolean result = (rootOne.val == rootTwo.val)&&isMirror(rootOne.left, rootTwo.right)&&isMirror(rootOne.right, rootTwo.left) ;
+        return result;   
+    }
+    
+
+}

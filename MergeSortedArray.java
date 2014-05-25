1d44a9b760aa0e2a721d3fe20bef846978de4cf7
givenwong <givenwong@gmail.com>
2014-05-25 00:20:58 -0500

    Merge sorted Array

---
+++ MergeSortedArray.java
@@ -0,0 +1,29 @@
+public class MergeSortedArray  {
+    public void merge(int A[], int m, int B[], int n) {
+        
+       while(m > 0 && n > 0){
+            if(A[m-1] > B[n-1]){
+                A[m+n-1] = A[m-1];
+                m--;
+            }else{
+                A[m+n-1] = B[n-1];
+                n--;
+            }
+        }
+        
+        while(m > 0){
+            A[m+n-1] = A[m-1];
+            m--;
+        } 
+ 
+        while(n > 0){
+            A[m+n-1] = B[n-1];
+            n--;
+        }
+        
+        
+        
+        
+        
+    }
+}

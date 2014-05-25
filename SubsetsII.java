90ad758a88a60fd5f1fcf726251a12d341295dc9
givenwong <givenwong@gmail.com>
2014-05-25 16:38:23 -0500

    SubsetsII

---
+++ SubsetsII.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class SubsetsII {
+    public ArrayList<ArrayList<Integer>> subsetsWithDup(int[] num) {
+        
+        int[] S = num;
+        
+        if (S == null)
+		return null;
+ 
+	Arrays.sort(S);
+ 
+	ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+ 
+	for (int i = 0; i < S.length; i++) {
+		ArrayList<ArrayList<Integer>> temp = new ArrayList<ArrayList<Integer>>();
+ 
+		//get sets that are already in result
+		for (ArrayList<Integer> a : result) {
+			temp.add(new ArrayList<Integer>(a));
+		}
+ 
+		//add S[i] to existing sets
+		for (ArrayList<Integer> a : temp) {
+			a.add(S[i]);
+		}
+ 
+		//add S[i] only as a set
+		ArrayList<Integer> single = new ArrayList<Integer>();
+		single.add(S[i]);
+		temp.add(single);
+ 
+		result.addAll(temp);
+	}
+ 
+	//add empty set
+	result.add(new ArrayList<Integer>());
+    
+    HashSet<ArrayList<Integer>> resultSet = new HashSet<ArrayList<Integer>>(result);
+    result = new ArrayList<ArrayList<Integer>>(resultSet);
+    
+	return result;
+    }
+}

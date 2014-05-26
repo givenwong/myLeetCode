a7e4afe925ac5f5dca90e7293e702363bb504c76
givenwong <givenwong@gmail.com>
2014-05-26 18:11:19 -0500

    Longest Consecutive Seq

---
+++ LongestConsecutiveSequence.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class LongestConsecutiveSequence {
+    public int longestConsecutive(int[] num) {
+     int max =1;
+     
+     HashSet<Integer> mySet = new HashSet<Integer>(); 
+     for(int e : num)
+         mySet.add(e);
+         
+      for(int e : num){
+          int left = e-1;
+          int right = e+1;
+          int count =1;
+          
+          while(mySet.contains(left)){
+              mySet.remove(new Integer(left));
+              left--;
+              count++;
+             
+          }
+          
+          while(mySet.contains(right)){
+              mySet.remove(new Integer(right));
+              right++;
+              count++;
+              
+          }
+          
+          max = (max > count)?max:count;
+      }    
+     
+     return max;
+    }
+}

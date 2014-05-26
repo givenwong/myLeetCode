0a869924def961c0b88fee8862b0a63ad32a35f7
givenwong <givenwong@gmail.com>
2014-05-26 15:23:25 -0500

    stock

---
+++ BestTimetoBuyandSellStock.java
@@ -0,0 +1,29 @@
+public class BestTimetoBuyandSellStock {
+    public int maxProfit(int[] prices) {
+        
+        if(prices == null || prices.length == 0 ||prices.length == 1)
+        return 0;
+        
+        int[] diff = new int[prices.length-1];
+        
+        
+        for(int i =0; i < prices.length-1; i++)
+            diff[i] = prices[i+1] - prices[i];
+        
+        int max = Integer.MIN_VALUE;
+        int sum =0;
+        int scanner = 0;
+        
+        while(scanner < prices.length-1 ){
+            sum += diff[scanner];
+            max = (max> sum)?max:sum;
+            
+            if(sum < 0)
+            sum =0;
+            
+            scanner++;
+        }
+        
+        return (max>0)?max:0;
+    }
+}

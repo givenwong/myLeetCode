9358ce9e8eb9b60024ab8cb9da82bf282d6f1cb7
givenwong <givenwong@gmail.com>
2014-05-26 17:04:08 -0500

    Binary Tree Z level order traversal

---
+++ BinaryTreeZigzagLevelOrderTraversal.java
@@ -0,0 +1,103 @@
+import java.util.ArrayList;
+
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class BinaryTreeZigzagLevelOrderTraversal {
+	  public ArrayList<ArrayList<Integer>> zigzagLevelOrder(TreeNode root) {
+	        
+	        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+	        ArrayList<TreeNode> myQueue = new ArrayList<TreeNode>();
+	        
+	        if(root == null)
+	        return result;
+	        
+	        myQueue.add(root);
+	        ArrayList<Integer> first = new ArrayList<Integer>();
+	        first.add(root.val);
+	        result.add(first);
+	        
+	        int thisLevel = 1;
+	        int nextLevel = 0;
+	        boolean startFromLeft = true;
+	        
+	        while(myQueue.size()>0){
+	    
+	            TreeNode top = myQueue.get(0);
+	            myQueue.remove(0);
+	            
+	        
+	            if(top.left != null)
+	            myQueue.add(top.left);
+	            if(top.right != null)
+	            myQueue.add(top.right);
+	         
+	            
+	            if(thisLevel ==1){
+	            	
+	             startFromLeft = (!startFromLeft);	
+	             if(top.left != null && top.right != null)    
+	             thisLevel = nextLevel+2;
+	             if((top.left != null && top.right == null)||(top.left == null && top.right != null) )    
+	             thisLevel = nextLevel+1;
+	             if(top.left == null && top.right == null)    
+	             thisLevel = nextLevel;
+	             nextLevel =0;
+	             ArrayList<Integer> temp = new ArrayList<Integer>();
+	            
+	            if(startFromLeft){ 
+	                
+	            for(TreeNode a: myQueue){
+	             temp.add(a.val);
+	            }
+	                
+	            }else{
+	             for(int i = myQueue.size()-1; i>=0 ;i--){
+	             temp.add(myQueue.get(i).val);
+	            }
+	            }
+	            
+	            if(temp.size() >0)
+	            result.add(temp);
+	            
+	            }else{
+	                thisLevel--;
+	             
+	             if(top.left != null && top.right != null)    
+	             nextLevel = nextLevel+2;
+	             if((top.left != null && top.right == null)||(top.left == null && top.right != null) )    
+	             nextLevel = nextLevel+1;
+	         
+	            }         
+	            
+	        }
+	        
+	     return result;
+	    }
+    
+    
+ public static void main(String[] args){
+    	
+    	Solution  mySolution = new Solution();
+    	
+    	TreeNode one = new TreeNode(1);
+    	TreeNode two = new TreeNode(2);
+    	TreeNode three = new TreeNode(3);
+    	TreeNode four = new TreeNode(4);
+    	TreeNode five = new TreeNode(5);
+    	one.left=two;
+    	one.right=three;
+    	two.left = four;
+    	two.right= five;
+    	
+    	ArrayList<ArrayList<Integer>> myResult = mySolution.zigzagLevelOrder(one);
+    	System.out.println(myResult);
+        
+    }
+}

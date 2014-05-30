af1ba73ed04f205f6b7a69c3efa83a9572721457
givenwong <givenwong@gmail.com>
2014-05-29 20:15:26 -0500

    Gas STATION

---
+++ GasStation.java
@@ -0,0 +1,31 @@
+public class GasStation {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+     
+     int[] diff = new int[gas.length];
+     int[] sum = new int[gas.length];
+    
+     for(int i = 0; i< gas.length;i++)
+         diff[i] = gas[i] - cost[i];
+    
+    int temp =0;
+    for(int i = 0; i< gas.length;i++)
+         temp += diff[i];
+         
+    if(temp <0)
+    return -1;
+    
+    
+    int seqSum =0;
+    int startIndex =0;
+    for(int i =0; i< gas.length;i++){
+        seqSum += diff[i];
+        if(seqSum <0){
+            startIndex = i+1;
+            seqSum =0;
+        }
+    }
+    
+    return startIndex;
+  
+    }
+}

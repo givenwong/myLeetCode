ed46f1732ded16fa69744235eb465926594a89e9
givenwong <givenwong@gmail.com>
2014-05-29 23:25:31 -0500

    Clone Graph

---
+++ CloneGraph.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+class UndirectedGraphNode {
+      int label;
+      List<UndirectedGraphNode> neighbors;
+      UndirectedGraphNode(int x) { label = x; neighbors = new ArrayList<UndirectedGraphNode>(); }
+  };
+ 
+public class CloneGraph {
+    public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
+        
+        if(node == null)
+        return null;
+        
+         ArrayList<UndirectedGraphNode> queue = new ArrayList<UndirectedGraphNode>();
+         HashMap<UndirectedGraphNode,UndirectedGraphNode> map = new  HashMap<UndirectedGraphNode,UndirectedGraphNode>();
+         
+         queue.add(node);
+         UndirectedGraphNode copy = new UndirectedGraphNode(node.label);
+         map.put(node,copy);
+         
+         while(queue.size()>0){
+             UndirectedGraphNode current = queue.get(0);
+             queue.remove(0);
+             ArrayList<UndirectedGraphNode> neighbors = (ArrayList)current.neighbors;
+             
+             for(UndirectedGraphNode a: neighbors){
+                 if(!map.containsKey(a)){
+                     queue.add(a);
+                     UndirectedGraphNode temp = new UndirectedGraphNode(a.label);
+                     map.put(a,temp);
+                     map.get(current).neighbors.add(temp);
+                     
+                 }else{
+                     map.get(current).neighbors.add(map.get(a));
+                 }
+             }
+             
+             
+         }
+         
+         return copy;
+    
+    }
+}

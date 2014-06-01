f049294b512d59a0d0db989b29ddf2db89a23fbd
givenwong <givenwong@gmail.com>
2014-05-31 20:00:46 -0500

    Recover Binary Search Tree

---
+++ RecoverBinarySearchTree.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class RecoverBinarySearchTree {
+    
+    TreeNode p,q;
+    TreeNode pre;
+    
+    public void recoverTree(TreeNode root) {
+    if (root == null) return;
+      getWrong(root);
+      int tmp = p.val;
+      p.val = q.val;
+      q.val = tmp;
+    }
+    
+    public void getWrong(TreeNode root) {
+        if (root == null) return;
+        getWrong(root.left);
+        if (pre != null && root.val < pre.val) {
+        if (p == null) {
+        p = pre;
+        q = root;
+         }else q = root;
+        }
+        pre = root;
+        getWrong(root.right);
+    }
+}
